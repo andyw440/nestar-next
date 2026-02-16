3663e44f206a30b95d448c4425111440e3d78e28
andyw440 <nozimovasror47@gmail.com>
2026-02-16 17:00:01 +0900

    fix: modify Websocket CHatting Client final stage

--- apollo/client.ts
+++ apollo/client.ts
@@ -7,6 +7,7 @@ import { onError } from '@apollo/client/link/error';
 import { getJwtToken } from '../libs/auth';
 import { TokenRefreshLink } from 'apollo-link-token-refresh';
 import { sweetErrorAlert } from '../libs/sweetAlert';
+import { socketVar } from './store';
 let apolloClient: ApolloClient<NormalizedCacheObject>;
 
 // Bu funksiya har bir GraphQL so'roviga Authorization headerini qo'shib beradi.
@@ -36,7 +37,9 @@ class LoggingWebSocket {
 	private socket: WebSocket;
 
 	constructor(url: string) {
-		this.socket = new WebSocket(url);
+		this.socket = new WebSocket(`${url}?token=${getJwtToken()}`);
+		socketVar(this.socket);
+
 
 		this.socket.onopen = () => {
 			console.log('WebSocket connection!');
